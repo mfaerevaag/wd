0a6efb1d8125ba35840ea62c08b194b29e7cf6b1
Markus Færevaag <mf@bitblueprint.com>
2013-11-12 17:49:12 +0100

    Inlined colors

--- install.sh
+++ install.sh
@@ -12,11 +12,6 @@ DIR=$BIN/wd
 REPO=https://github.com/mfaerevaag/wd.git
 ZSHRC=$HOME/.zshrc
 
-# colors
-BLUE="\033[96m"
-RED="\033[91m"
-NOC="\033[m"
-
 # make temporary log file
 LOG="$(mktemp -t wd_install.XXXXXXXXXX)" || exit 1
 exec 2> $LOG
@@ -46,19 +41,19 @@ then
     rm -rf $LOG
 
     # finish
-    echo "$BLUE              _ "
-    echo "$BLUE             | |"
-    echo "$BLUE __      ____| |"
-    echo "$BLUE \ \ /\ / / _\` |"
-    echo "$BLUE  \ V  V / (_| |"
-    echo "$BLUE   \_/\_/ \__,_|"
-    echo "$BLUE                "
-    echo "... is now installed. $NOC"
+    echo "\033[96m"'              _ '
+    echo "\033[96m"'             | |'
+    echo "\033[96m"' __      ____| |'
+    echo "\033[96m"' \ \ /\ / / _` |'
+    echo "\033[96m"'  \ V  V / (_| |'
+    echo "\033[96m"'   \_/\_/ \__,_|'
+    echo "\033[96m"'                '
+    echo '... is now installed.'"\033[m"
     echo "Remember to open new zsh to load alias."
     echo "For more information on usage, see README. Enjoy!"
 else
     # ops
-    echo "$RED\nOps! An error occured: $NOC"
+    echo "\033[91m\n"'Ops! An error occured: '"\033[m"
     cat $LOG
     echo "\nIt would be great of you to file an issue"
     echo "at GitHub so I can fix it asap. Sorry!"
