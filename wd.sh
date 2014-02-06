f4f2f8b7b2154d08a4a33a4778ec4af92f0659a7
Markus Faerevaag <mf@bitblueprint.com>
2014-02-06 13:19:06 +0100

    Removed unecessary output

--- wd.sh
+++ wd.sh
@@ -50,12 +50,12 @@ wd_warp()
             wd_print_msg $YELLOW "Warping to current directory?"
         else
             (( n = $#1 - 1 ))
-            wd_print_msg $BLUE "Warping..."
+            #wd_print_msg $BLUE "Warping..."
             cd -$n > /dev/null
         fi
     elif [[ ${points[$1]} != "" ]]
     then
-        wd_print_msg $BLUE "Warping..."
+        #wd_print_msg $BLUE "Warping..."
         cd ${points[$1]}
     else
         wd_print_msg $RED "Unkown warp point '$1'"
@@ -127,7 +127,7 @@ wd_print_msg()
 
 wd_print_usage()
 {
-		print "Usage: wd [add|-a|--add] [rm|-r|--remove] [ls|-l|--list] <point>"
+    print "Usage: wd [add|-a|--add] [rm|-r|--remove] [ls|-l|--list] <point>"
     print "\nCommands:"
     print "\t add \t Adds the current working directory to your warp points"
     print "\t add! \t Overwrites existing warp point"
@@ -161,39 +161,39 @@ else
 
     for i
     do
-		    case "$i"
-		        in
-			      -a|--add|add)
+        case "$i"
+            in
+            -a|--add|add)
                 wd_add $2 false
-				        shift
+                shift
                 shift
                 break
                 ;;
             -a!|--add!|add!)
                 wd_add $2 true
-				        shift
+                shift
                 shift
                 break
                 ;;
-			      -r|--remove|rm)
-				        wd_remove $2
+            -r|--remove|rm)
+                wd_remove $2
                 shift
-				        shift
+                shift
                 break
                 ;;
-			      -l|--list|ls)
-				        wd_list_all
-				        shift
+            -l|--list|ls)
+                wd_list_all
+                shift
                 break
                 ;;
-			      -h|--help|help)
-				        wd_print_usage
-				        shift
+            -h|--help|help)
+                wd_print_usage
+                shift
                 break
                 ;;
-			      -s|--show|show)
-				        wd_show
-				        shift
+            -s|--show|show)
+                wd_show
+                shift
                 break
                 ;;
             *)
@@ -201,9 +201,9 @@ else
                 shift
                 break
                 ;;
-			      --)
-				        shift; break;;
-		    esac
+            --)
+                shift; break;;
+        esac
     done
 fi
 
