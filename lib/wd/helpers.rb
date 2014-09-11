3e45f44dc0db7357815b0c6c4e5e3243067d4ff4
Markus Faerevaag <mafaer@gmail.com>
2014-09-11 11:13:49 +0200

    Added some helper methods

--- lib/wd/helpers.rb
+++ lib/wd/helpers.rb
@@ -1,8 +1,26 @@
 module Wd
 
-  def self.print_and_exit(content)
+  def self.print_and_exit(content, code = 1)
     puts content
-    exit 1
+    exit code
+  end
+
+  def self.print_point(name, path)
+    path.gsub! ENV['HOME'], '~'
+    printf "\t%s > %s\n", name, path
+  end
+
+  def self.print_points(points)
+    longest_key = points.keys.max { |a, b| a.length <=> b.length }
+
+    points.each do |name, path|
+      path.gsub! ENV['HOME'], '~'
+      printf "\t%-#{longest_key.length}s > %s\n", name, path
+    end
+  end
+
+  def self.pwd
+    ENV['PWD'].gsub ENV['HOME'], '~'
   end
 
 end
