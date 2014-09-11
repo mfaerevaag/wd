bb63b23c55905e6a1d9703837cfa1aba878f42bc
Markus Faerevaag <mafaer@gmail.com>
2014-09-11 11:13:32 +0200

    Added Points class

--- lib/wd/points.rb
+++ lib/wd/points.rb
@@ -1,70 +1,112 @@
 require 'fileutils'
 require 'csv'
 
+require 'wd/error'
+
 module Wd
   class Points
+    class << self
 
-    def initialize(config_file)
-      @config_file = config_file
-
-      @points = read_points
-    end
-
-    def all
-      @points
-    end
-
-    def has?(key)
-      @points.has_key? key
-    end
-
-    def add!(key, value, options={ force: false })
-      key = key.to_sym
-
-      if !@points.has? key or options[:force]
-        @points[key] = value
-        true
-      else
-        false
-      end
-    end
-
-    def remove!(key)
-      if @points.delete key
-        true
-      else
-        false
-      end
-    end
-
-    def persist!
-      # lazy create config file
-      unless File.file?(config_file)
-        FileUtils.touch(config_file)
+      def all
+        points
       end
 
-      CSV.open(@config_file, 'wb') do |csv|
-        @points.each do |key, value|
-          csv << [key.to_s, value]
+      def has?(name)
+        points.has_key? name
+      end
+
+      def get(options = { name: nil, path: nil })
+        if !options[:name].nil?
+          name = options[:name]
+
+          unless self.has? name
+            raise Wd::UnknownPoint.new name
+          end
+
+          path = points[name]
+          unless File.directory? path
+            raise Wd::OrphanedPoint.new name
+          end
+          path
+
+        elsif !options[:path].nil?
+          path = options[:path]
+
+          unless points.has_value? path
+            raise Wd::PointNotFound.new
+          end
+
+          points.select { |k,v| v == path }
         end
       end
-    end
 
-    private
+      def add!(name, options={ force: false })
+        if Wd::Points::has?(name) && !options[:force]
+          raise Wd::PointAlreadyExists.new name
+        end
 
-    def read_points
-      points = {}
-      return points  unless File.file?(@config_file)
+        points[name] = ENV['PWD']
 
-      CSV.foreach(@config_file) do |row|
-        key = row.shift.to_sym
-        value = row.shift
-
-        points[key] = value
+        self.persist!
       end
 
-      points
-    end
+      def remove!(names)
+        if names.is_a? Symbol
+          names = [ names ]
+        end
 
+        # check if names exists
+        names.each do |name|
+          unless self.has? name
+            raise Wd::UnknownPoint.new name
+          end
+        end
+
+        # remove
+        names.each do |name|
+          points.delete name
+        end
+
+        self.persist!
+      end
+
+      def persist!
+        config_file = Wd::opts[:config]
+
+        # lazy create config file
+        unless File.file?(config_file)
+          FileUtils.touch(config_file)
+        end
+
+        CSV.open(config_file, 'wb') do |csv|
+          points.each do |key, value|
+            csv << [key.to_s, value]
+          end
+        end
+      end
+
+      private
+
+      def points
+        @points ||= read_points
+      end
+
+      def read_points
+        points = {}
+        config = Wd::opts[:config]
+
+        return points unless File.file?(config)
+
+        CSV.foreach(config) do |row|
+          key = row.shift.to_sym
+          value = row.shift
+
+          points[key] = value
+        end
+
+        points
+      end
+
+    end
   end
 end
