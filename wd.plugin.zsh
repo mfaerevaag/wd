f875d5c6a7d7b9b13988cd4911d5de1b6bc9b969
p1r473 <subwayjared@gmail.com>
2024-05-08 15:02:23 -0400

    wd_browse plugin to search with fzf & more colorful output (#120)
    Co-authored-by: alpha-tango-kilo <git@heyatk.com>

--- wd.plugin.zsh
+++ wd.plugin.zsh
@@ -8,3 +8,8 @@
 # @github.com/mfaerevaag/wd
 
 eval "wd() { source '${0:A:h}/wd.sh' }"
+wd > /dev/null
+# Register the function as a Zsh widget
+zle -N wd_browse
+# Bind the widget to a key combination
+bindkey '^G' wd_browse
