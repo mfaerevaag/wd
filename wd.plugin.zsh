6739e39833a64dbcdc25901e0e8698e0ca9d6dff
alpha-tango-kilo <git@heyatk.com>
2024-05-08 21:05:08 +0100

    Handle $0 according to the Zsh plugin standard
    For details and rationale, see:
    https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
    
    https://github.com/ohmyzsh/ohmyzsh/commit/2d32e9be668672c110405aa2952ab8adaf374856

--- wd.plugin.zsh
+++ wd.plugin.zsh
@@ -7,6 +7,11 @@
 #
 # @github.com/mfaerevaag/wd
 
+# Handle $0 according to the standard:
+# # https://zdharma-continuum.github.io/Zsh-100-Commits-Club/Zsh-Plugin-Standard.html
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
 eval "wd() { source '${0:A:h}/wd.sh' }"
 wd > /dev/null
 # Register the function as a Zsh widget
